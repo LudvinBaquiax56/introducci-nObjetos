43f11e18bad297c0e35e197d71af6b657427ac87
LudvinBaquiax56 <vasqueznicolas56@gmail.com>
2021-06-16 14:16:20 -0600

    Clase Fraccion creada con sus respectivos metodos

---
+++ src/Ejercicio6/Fraccion.java
@@ -0,0 +1,78 @@
+package src.Ejercicio6;
+
+public class Fraccion {
+
+	private int numerador1;
+	private int denominador1;
+
+	private int numerador2;
+	private int denominador2;
+
+	public Fraccion(int numerador1, int denominador1, int numerador2, int denominador2){
+		this.numerador1 = numerador1;
+		this.denominador1 = denominador1;
+		this.numerador2 = numerador2;
+		this.denominador2 = denominador2;
+	}
+
+	public int getNumerador1(){
+		return numerador1;
+	}
+
+	public void setNumerador1(int numerador1){
+		this.numerador1 = numerador1;
+	}
+
+	public int getNumerador2(){
+		return numerador1;
+	}
+
+	public void setNumerador2(int numerador1){
+		this.numerador1 = numerador1;
+	}
+
+	public void suma(){
+		int nResultado;
+		int dResultado;
+		if (denominador1 == denominador2) {
+			dResultado = denominador1;
+			nResultado = numerador1 + numerador2;
+		} else {
+			dResultado = denominador1*denominador2;
+			nResultado = ((int)(dResultado/denominador1) * numerador1) + ((int)(dResultado/denominador2) * numerador2); 
+		}
+		System.out.println("La suma es " + nResultado + "/" + dResultado);
+	}
+
+	public void resta(){
+		int nResultado;
+		int dResultado;
+		if (denominador1 == denominador2) {
+			dResultado = denominador1;
+			nResultado = numerador1 - numerador2;
+		} else {
+			dResultado = denominador1*denominador2;
+			nResultado = ((int)(dResultado/denominador1) * numerador1) - ((int)(dResultado/denominador2) * numerador2); 
+		}
+		System.out.println("La resta es " + nResultado + "/" + dResultado);
+	}
+
+	public void multiplicacion(){
+		int nResultado;
+		int dResultado;
+		
+		dResultado = denominador1*denominador2;
+		nResultado = numerador1*numerador2;
+		System.out.println("La multiplicacion es " + nResultado + "/" + dResultado);
+	}
+
+	public void division(){
+		int nResultado;
+		int dResultado;
+		
+		dResultado = denominador1*numerador2;
+		nResultado = numerador1*denominador2;
+		System.out.println("La division es " + nResultado + "/" + dResultado);
+	}
+
+}
--- src/Principal.java
+++ src/Principal.java
@@ -5,6 +5,7 @@ import src.Ejercicio2.*;
 import src.Ejercicio3.*;
 import src.Ejercicio4.*;
 import src.Ejercicio5.*;
+import src.Ejercicio6.*;
 
 public class Principal {
 
@@ -13,32 +14,47 @@ public class Principal {
 	}
 
 	public Principal (){
-		Contador a = new Contador();
-		a.aumentarContador();
-		System.out.println(a.getContador());
-		a.aumentarContador(5);
-		System.out.println(a.getContador());
-		a.disminuirContador();
-		System.out.println(a.getContador());
-
+		System.out.println(".....................................................");
+		System.out.println("Ejercicio 1");
+		Contador primerContador = new Contador();
+		Contador segundoContador = primerContador;
+		segundoContador.setContador(3);
+		System.out.println("Primer contador " + primerContador.getContador());
+		System.out.println("Segundo contador " + segundoContador.getContador());
+		
+		System.out.println(".....................................................");
+		System.out.println("Ejercicio 2");
 		Cuadrado b = new Cuadrado(3);
 		System.out.println(b.calcularArea());
 		System.out.println(b.calcularPerimetro());
 
-
+		System.out.println(".....................................................");
+		System.out.println("Ejercicio 3");
 		Circulo c = new Circulo(3);
 		System.out.println(c.calcularArea());
 		System.out.println(c.calcularPerimetro());
 
+		System.out.println(".....................................................");
+		System.out.println("Ejercicio 4");
 		Rectangulo d = new Rectangulo(5,6);
 		System.out.println(d.calcularArea());
 		System.out.println(d.calcularPerimetro());
 
+		System.out.println(".....................................................");
+		System.out.println("Ejercicio 5");
 		Libro libro = new Libro("La divina comedia","Mexican", "Dante", 2007);
-		libro.mostrarDatos2();
-		libro.prestamo();
-		libro.mostrarDatos2();
-		libro.devolucion();
-		libro.mostrarDatos2();
+		//libro.mostrarDatos2();
+		//libro.prestamo();
+		//libro.mostrarDatos2();
+		//libro.devolucion();
+		//libro.mostrarDatos2();
+		
+		System.out.println(".....................................................");
+		System.out.println("Ejercicio 6");
+		Fraccion fra = new Fraccion(1,7,1,4);
+		fra.suma();
+		fra.resta();
+		fra.multiplicacion();
+		fra.division();
 	}
 }
